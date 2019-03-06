f9332136422c02f23f3cb20b62122e6139f4d02b
lisaiqi <864878254@qq.com>
2019-03-06 15:44:12 +0800

    s

--- src/pages/test/Test.js
+++ src/pages/test/Test.js
@@ -1,23 +1,41 @@
 
 import React, { Component } from 'react';
- 
 class Test extends Component {
     constructor (props) {
         super(props)
         console.log(props)
+        this.state={count:0}
     }
  
-   render() {
-       return (
-           <div id="test-container">
-               <p>search:{this.props.location.search} </p>
-               <p>state:{this.props.location.state.mold} </p>
-               <p>state:{this.props.location.aa} </p>
-               <div onClick={() =>  this.props.history.goBack()}>返回上一页</div>
-               <div onClick={() => this.props.history.push('/member/12')}>message页面</div>
-           </div>
-       );
-   }
+    minus(i){
+        i--;
+        this.setState({
+            count:i
+        })
+    }
+    add(i){
+        i++
+        this.setState({
+            count:i
+        })
+        
+    }
+    render() {
+        return (
+            <div id="test-container">
+                    <p>search:{this.props.location.search} </p>
+                    <p>state:{this.props.location.state.mold} </p>
+                    <p>state:{this.props.location.aa} </p>
+                    <div onClick={() =>  this.props.history.goBack()}>返回上一页</div>
+                    <div onClick={() => this.props.history.push('/member/12')}>message页面</div>
+                    <div>  
+                        <button onClick={this.minus.bind(this,this.state.count)}>减</button>
+                        <span>{this.state.count}</span>
+                        <button onClick={this.add.bind(this,this.state.count)}>加</button>
+                    </div>
+            </div>
+        );
+    }
 }
 
 export default Test;
